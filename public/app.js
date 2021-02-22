38c314e94e7f871170c2e3f0ed5f79533d1209f6
karanbir-singh <karans1723@gmail.com>
2021-02-22 14:14:15 +0100

    Implemented the creation of messages

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ clientSocket.on('connect', (socket) => {
     console.log('Connesso');
 
     btnPing.onclick = () => {
-        clientSocket.emit('ping',{});
+        clientSocket.emit('ping', {});
     };
 });
 
@@ -21,3 +21,42 @@ clientSocket.on('message', (serverMsg) => {
 clientSocket.on('pong', () => {
     console.log('Pong from server');
 });
+
+//Displays the massage
+function createMassage(userId, msg) {
+    //Item
+    const listItem = document.createMassage("li");
+    listItem.class = "out";
+
+    //Img utente
+    const userImgDiv = document.createMassage("div");
+    userImgDiv.class = "chat-img";
+    const userImg = document.createMassage("img");
+    userImg.alt = "Avtar";
+    userImg.src = "https://bootdey.com/img/Content/avatar/avatar1.png";
+
+    userImgDiv.appendChild(userImg);
+
+    //Body
+    const body = document.createElement("div");
+    body.class = "chat-body";
+
+    //Contenitore messaggio
+    const messageDiv = document.createElement("div");
+    messageDiv.class = "chat-message";
+
+    //Utente
+    const user = document.createElement("h5");
+    user.innerText = userId;
+
+    //Messaggio
+    const message = document.createElement("p");
+    message.innerText = msg;
+
+    messageDiv.appendChild(user);
+    messageDiv.appendChild(message);
+
+    body.appendChild(messageDiv);
+
+    listItem.appendChild(body);
+}
